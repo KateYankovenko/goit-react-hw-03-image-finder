d1657d067c3cb219ba5833118daf901214c70d93
Kate Yankovenko <k.yankovenko@ukr.net>
2022-08-08 13:55:29 +0200

    styles

--- src/services/pixabay-api.js
+++ src/services/pixabay-api.js
@@ -1,1 +1,31 @@
-// const API_KEY = `27730818-b3b91ab106a7108cef17bdc3c`;
+import axios from 'axios';
+import { Notify } from 'notiflix';
+
+const API_KEY = `27730818-b3b91ab106a7108cef17bdc3c`;
+
+
+export default async function fetchResult(url, query, page) {
+  const response = await axios
+    .get(url, {
+      params: {
+        key: API_KEY,
+        q: query,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: `true`,
+        per_page: 12,
+        page: page,
+      },
+    })
+    .catch(function (error) {
+      console.log(error);
+      return `No results with query "${query}"`;
+    });
+  const images = await response.data;
+  if (images.totalHits === 0) {
+    Notify.warning(`Sorry, no images found with query "${query}"`);
+  }
+  return images;
+}
+
+ 
